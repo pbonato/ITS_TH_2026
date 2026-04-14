8407e178a59183e5771fdcee4035adb68d425736
Bonato Pierantonio <info@bonato.it>
2026-04-14 12:23:06 +0200

    upèdate classe cliente

--- TechnicalIssue/Intervento.cs
+++ TechnicalIssue/Intervento.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace TechnicalIssue
-namespace TechnicalIssue.controller
 {
     public class Intervento
     {
@@ -16,7 +16,8 @@ namespace TechnicalIssue.controller
         public string Stato { get; set; }
         public int IdCliente { get; set; }
 
-        private string connString = @"Server=DESKTOP-B6PIEOF\SQLEXPRESS;Database=TestInterventiTecnici;Trusted_Connection=True;";
+        public static string connectionString = ConfigurationManager.ConnectionStrings["TechincalIssueDB"].ConnectionString;
+
 
         //Tutti gli interventi di un cliente
         public List<Intervento> GetInterventiByCliente(int cliente)
@@ -47,47 +48,6 @@ namespace TechnicalIssue.controller
             return lista;
         }
 
-        //Tutti gli interventi 
-        public List<Intervento> GetInterventi()
-        {
-            List<Intervento> lista = new List<Intervento>();
-
-            using (SqlConnection conn = new SqlConnection(connString))
-            {
-                conn.Open();
-                string query = "SELECT * FROM Interventi";
-                SqlCommand cmd = new SqlCommand(query, conn);
-        public static List<Intervento> GetInterventiByCliente(int idCliente)
-        {
-            List<Intervento> lista = new List<Intervento>();
-
-            string connectionString = "SERVER=DESKTOP-BAVMQE9\\SQLEXPRESS;DATABASE=TestInterventiTecnici;INTEGRATED SECURITY=True;";
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                string query = "SELECT * FROM Interventi WHERE IdCliente = @idCliente";
-
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@idCliente", idCliente);
-
-                con.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Intervento i = new Intervento();
-                    i.IdIntervento = (int)reader["IdIntervento"];
-                    i.DataIntervento = (DateTime)reader["DataIntervento"];
-                    i.Descrizione = reader["Descrizione"].ToString();
-                    i.Tecnico = reader["Tecnico"].ToString();
-                    i.Stato = reader["Stato"].ToString();
-
-                    lista.Add(i);
-                }
-            }
-
-            return lista;
-        }
         public static List<Intervento> GetInterventi()
         {
             List<Intervento> lista = new List<Intervento>();
@@ -114,20 +74,6 @@ namespace TechnicalIssue.controller
                 }
             }
 
-            return lista;
-        }
-
-                    Intervento i = new Intervento();
-                    i.IdIntervento = (int)reader["IdIntervento"];  // Assicurati che ci sia!
-                    i.IdCliente = (int)reader["IdCliente"];        // Serve per il dropdown clienti
-                    i.DataIntervento = (DateTime)reader["DataIntervento"];
-                    i.Descrizione = reader["Descrizione"].ToString();
-                    i.Tecnico = reader["Tecnico"].ToString();
-                    i.Stato = reader["Stato"].ToString();
-
-                    lista.Add(i);
-                }
-            }
             return lista;
         }
     }
